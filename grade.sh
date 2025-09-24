aa0bd1f164f1088f809b7b4d9f89aabb974cf686
doruche <doruche18@outlook.com>
2025-09-24 22:27:54 +0800

    update

--- grade.sh
+++ grade.sh
@@ -1,6 +1,3 @@
 #! /bin/zsh
 
-rm -rf ci-user
-git clone https://github.com/LearningOS/rCore-Tutorial-Checker.git ci-user
-git clone https://github.com/LearningOS/rCore-Tutorial-Test.git ci-user/user
 cd ci-user && make test CHAPTER=$1
